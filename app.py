d45ecd5ef7f078064932871a480585217c03ae95
rohan25-rawat <rawatrohan692@gmail.com>
2026-07-30 11:11:40 +0530

    Update background image in app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.title("AI RESUME GENERATOR")
 st.write("""This app helps user to build customized Professional
 Resume with Latest Job apply links""")
 
-st.image("bg1.jpg")
+st.image("bg.jpg")
 st.sidebar.title("Fill Important Details")
 st.sidebar.image("bg1.jpg")
 
