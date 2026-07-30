8691ad0960c8b0a84aad066ed3037a0f1c421f5a
rohan25-rawat <rawatrohan692@gmail.com>
2026-07-30 10:54:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ GOOGLE_API_KEY= st.sidebar.text_input("Gemini-API", type = "password")
 GROQ_API_KEY= st.sidebar.text_input("Groq-API", type = "password")
 TAVILY_API_KEY = st.sidebar.text_input("Tavily-API", type = "password")
 
-all_API = [TAVILY_API_KEY,GRoQ_API_KEY,GOOGLE_API_KEY]
+all_API = [TAVILY_API_KEY,GROQ_API_KEY,GOOGLE_API_KEY]
 if not all(all_API):
     st.error("Must give all keys")
     st.stop()
